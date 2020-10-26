d503357298cd18b370a1eccbfb2f032ecb6e3923
Pablo Garrido <pablogs9@gmail.com>
2020-10-26 10:48:20 +0100

    Added support for Teensy

--- src/arduino_transports.c
+++ src/arduino_transports.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#if defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY32)
+#if defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY32)
 #include "teensy_transports.c.in"
 #elif defined(ARDUINO_ARCH_OPENCR)
 #include "opencr_transports.c.in"
