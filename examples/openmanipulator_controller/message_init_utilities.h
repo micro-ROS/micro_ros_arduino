3a7adc1ab0b18eb5e76c39d129746ca534a79d5f
Pablo Garrido <pablogs9@gmail.com>
2020-11-13 10:32:41 +0100

    Updates
    Updates

--- examples/openmanipulator_controller/message_init_utilities.h
+++ examples/openmanipulator_controller/message_init_utilities.h
@@ -2,6 +2,7 @@
 
 #include <control_msgs/action/follow_joint_trajectory.h>
 #include <trajectory_msgs/msg/joint_trajectory_point.h>
+#include <trajectory_msgs/msg/joint_trajectory.h>
 #include <control_msgs/msg/joint_tolerance.h>
 #include <sensor_msgs/msg/joint_state.h>
 
@@ -9,6 +10,5 @@
 #include <rosidl_runtime_c/primitives_sequence_functions.h>
 #include <rosidl_runtime_c/primitives_sequence_functions.h>
 
-control_msgs__action__FollowJointTrajectory_FeedbackMessage * create_feedback_message();
-control_msgs__action__FollowJointTrajectory_SendGoal_Request *  create_goal_request();
 sensor_msgs__msg__JointState * create_joint_states_message();
+trajectory_msgs__msg__JointTrajectory * create_joint_trajectory_msg();
--- examples/openmanipulator_controller/openmanipulator_controller.ino
+++ examples/openmanipulator_controller/openmanipulator_controller.ino
@@ -19,78 +19,27 @@ OpenManipulator open_manipulator;
 rcl_allocator_t allocator;
 rclc_support_t support;
 rcl_node_t node;
-rcl_action_server_t follow_joint_trajectory_server;
 rclc_executor_t executor;
 rcl_timer_t timer;
 rcl_publisher_t joint_state_publisher;
-
-bool processing_goal = false;
-bool result_requested = false;
-
-control_msgs__action__FollowJointTrajectory_SendGoal_Request * goal_request;
-control_msgs__action__FollowJointTrajectory_GetResult_Request * result_request;
-control_msgs__action__FollowJointTrajectory_FeedbackMessage * feedback;
-control_msgs__action__FollowJointTrajectory_GetResult_Response * result_response;
-
-rcl_action_goal_info_t goal_info;
-rmw_request_id_t request_header;
-rcl_action_goal_handle_t * goal_handle;
+rcl_subscription_t joint_trajectory_subscriber;
 
 sensor_msgs__msg__JointState * join_states_msg;
+trajectory_msgs__msg__JointTrajectory * joint_trajectory_msg;
 
-void goal_callback(const void * msg, rmw_request_id_t * request){
-  control_msgs__action__FollowJointTrajectory_SendGoal_Request * ros_msg = (control_msgs__action__FollowJointTrajectory_SendGoal_Request * ) msg;
-
-  control_msgs__action__FollowJointTrajectory_SendGoal_Response ros_goal_response;
-  ros_goal_response.accepted = !open_manipulator.getMovingState();
-
-  rcl_action_send_goal_response(&follow_joint_trajectory_server, request, &ros_goal_response);
-
-  if (ros_goal_response.accepted) {
-
-    processing_goal = true;
-    
-    // Accept goal
-    goal_info.goal_id = ros_msg->goal_id;
-    goal_handle = rcl_action_accept_new_goal(&follow_joint_trajectory_server, &goal_info);
-
-    rcl_action_update_goal_state(goal_handle, GOAL_EVENT_EXECUTE);
-
-    rcl_action_goal_status_array_t c_status_array = rcl_action_get_zero_initialized_goal_status_array();
-    rcl_action_get_goal_status_array(&follow_joint_trajectory_server, &c_status_array);
-    rcl_action_publish_status(&follow_joint_trajectory_server, &c_status_array.msg);
-    
-    //Do the preparation
-    std::vector <double> velocities;
-    velocities.clear();
-    std::vector <double> accelerations;
-    accelerations.clear();
-    std::vector <double> efforts;
-    efforts.clear();
-    std::vector <double> target_angle;
-    target_angle.clear();
-    float duration;
-    
-    for(uint8_t i = 0; i < 4; i++){
-      //velocities.push_back(ros_msg->goal.trajectory.points.data[0].velocities.data[i]);
-      //accelerations.push_back(ros_msg->goal.trajectory.points.data[0].accelerations.data[i]);
-      //efforts.push_back(ros_msg->goal.trajectory.points.data[0].effort.data[i]);
-      target_angle.push_back(ros_msg->goal.trajectory.points.data[0].positions.data[i]);
-      duration = (float) ros_msg->goal.trajectory.points.data[0].time_from_start.sec + ((float) ros_msg->goal.trajectory.points.data[0].time_from_start.nanosec/1e9f);
-      open_manipulator.makeJointTrajectory(target_angle, duration);
-    }
-
-  } else {
-    //Not accepted
+void join_trajectory_callback(const void * msgin){
+  trajectory_msgs__msg__JointTrajectory * ros_msg = (trajectory_msgs__msg__JointTrajectory *) msgin;
+  
+  std::vector<double> positions;
+  for (size_t i = 0; i < 4; i++)
+  {
+    // positions[i] += 0.01 * ros_msg->points.data[0].velocities.data[i];
+    positions.push_back(ros_msg->points.data[0].velocities.data[i]);
   }
-}
-
-void result_callback(const void * msg, rmw_request_id_t * request){
-  // Prepare the result and the feedback
-  if(processing_goal){
-    result_requested = true;
-    request_header = *request;
-  }
+  
+  open_manipulator.makeJointTrajectory(positions, 0.2);
+  // positions[3] = 2.22;
+  
 }
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
@@ -110,10 +59,11 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
   }
 }
 
+
 void setup()
 {  
-  open_manipulator.initOpenManipulator(true);
-  delay(2000);
+  // open_manipulator.initOpenManipulator(true);
+  delay(1000);
 
   allocator = rcl_get_default_allocator();
   
@@ -124,87 +74,45 @@ void setup()
   node = rcl_get_zero_initialized_node();
   rclc_node_init_default(&node, "open_manipulator_node", "", &support);
 
-  // create action server
-  rclc_action_server_init_default(
-    &follow_joint_trajectory_server,
-    &node,
-    &support.clock,
-    ROSIDL_GET_ACTION_TYPE_SUPPORT(control_msgs, FollowJointTrajectory),
-    "/followjointrajectory");
-
   // create publisher
   rclc_publisher_init_default(
     &joint_state_publisher,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, JointState),
-    "/join_states");
+    "/joint_states");
+  
+  // create subscriber
+  rclc_subscription_init_best_effort(
+    &joint_trajectory_subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(trajectory_msgs, msg, JointTrajectory),
+    "/open_manipulator/joint_trajectory");
 
   // create timer,
   timer = rcl_get_zero_initialized_timer();
-  const unsigned int timer_timeout = 1000;
+  const unsigned int timer_timeout = 10;
   rclc_timer_init_default(
     &timer,
     &support,
     RCL_MS_TO_NS(timer_timeout),
     timer_callback);
 
+  // Creating messages
+  // joint_trajectory_msg = create_joint_trajectory_msg();
+  join_states_msg = create_joint_states_message();
+
   // create executor
   executor = rclc_executor_get_zero_initialized_executor();
-  rclc_executor_init(&executor, &support.context, 2, &allocator);
+  rclc_executor_init(&executor, &support.context, 3, &allocator);
 
-  unsigned int rcl_wait_timeout = 1000;   // in ms
+  unsigned int rcl_wait_timeout = 10;   // in ms
   rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout));
-
+	rclc_executor_add_subscription(&executor, &joint_trajectory_subscriber, joint_trajectory_msg, &join_trajectory_callback, ON_NEW_DATA);
   rclc_executor_add_timer(&executor, &timer);
-  rclc_executor_add_action_server(
-    &executor,
-    &follow_joint_trajectory_server,
-    goal_request,
-    result_request,
-    goal_callback,
-    result_callback);
-
-  // Creating messages
-  goal_request = create_goal_request();
-  result_request = control_msgs__action__FollowJointTrajectory_GetResult_Request__create();
-  feedback = create_feedback_message();
-  result_response = control_msgs__action__FollowJointTrajectory_GetResult_Response__create();
-  rosidl_runtime_c__String__assign(&result_response->result.error_string, "");
-
-  join_states_msg = create_joint_states_message();
 }
 
 void loop()
 { 
-  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-  open_manipulator.processOpenManipulator(millis()/1000.0);
-
-  if (processing_goal && result_requested){
-    for (size_t i = 0; i < feedback->feedback.desired.positions.capacity; i++){
-      feedback->feedback.desired.positions.data[i] = 0.0;
-      feedback->feedback.desired.velocities.data[i] = 0.0;
-      feedback->feedback.desired.accelerations.data[i] = 0.0;
-      feedback->feedback.desired.effort.data[i] = 0.0;
-
-      feedback->feedback.actual.positions.data[i] = 0.0;
-      feedback->feedback.actual.velocities.data[i] = 0.0;
-      feedback->feedback.actual.accelerations.data[i] = 0.0;
-      feedback->feedback.actual.effort.data[i] = 0.0;
-
-      feedback->feedback.error.positions.data[i] = 0.0;
-      feedback->feedback.error.velocities.data[i] = 0.0;
-      feedback->feedback.error.accelerations.data[i] = 0.0;
-      feedback->feedback.error.effort.data[i] = 0.0;
-    }
-    rcl_action_publish_feedback(&follow_joint_trajectory_server, feedback);
-  }
-
-  if (processing_goal && !open_manipulator.getMovingState()){
-    processing_goal = false;
-    if (result_requested){
-      result_response->status = action_msgs__msg__GoalStatus__STATUS_SUCCEEDED;
-      result_response->result.error_code = 0;
-      rcl_action_send_result_response(&follow_joint_trajectory_server, &request_header, &result_response);
-    }
-  }
+  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
+  // open_manipulator.processOpenManipulator(millis()/1000.0);
 }
