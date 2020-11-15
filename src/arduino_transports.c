bd8625356eab0850101cefc8157e1161046e75ae
Darko Lukic <lukicdarkoo@gmail.com>
2020-11-15 19:31:00 +0100

    Add Arduino Due support

--- src/arduino_transports.c
+++ src/arduino_transports.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/*
+TODO: We should do something about this
+
 #if defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY32)
 #include "teensy_transports.c.in"
 #elif defined(ARDUINO_ARCH_OPENCR)
@@ -8,6 +11,7 @@
 #else
 #error micro-ROS Library not supported for this platform
 #endif
+*/
 
 
 // TODO: This should be fixed
