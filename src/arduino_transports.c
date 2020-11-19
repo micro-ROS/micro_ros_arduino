d785b533489079b60ea08f77f733b7b528c26a45
Pablo Garrido <pablogs9@gmail.com>
2020-11-19 10:26:19 +0100

    Add support for Arduino Due and Arduino Zero (#35)
    * Add Arduino Due support
    
    * Add binary
    
    * Add support
    
    * Improve arduino_serial_platform_read
    
    * Add working example
    
    * Revert example
    
    * Revert example
    
    * New line
    
    * Add CI
    
    * Updates
    
    Co-authored-by: Darko Lukic <lukicdarkoo@gmail.com>

--- src/arduino_transports.c
+++ src/arduino_transports.c
@@ -5,10 +5,7 @@
 #include "teensy_transports.c.in"
 #elif defined(ARDUINO_ARCH_OPENCR)
 #include "opencr_transports.c.in"
-#else
-#error micro-ROS Library not supported for this platform
 #endif
-
 
 // TODO: This should be fixed
 #if defined(ARDUINO_TEENSY32)
