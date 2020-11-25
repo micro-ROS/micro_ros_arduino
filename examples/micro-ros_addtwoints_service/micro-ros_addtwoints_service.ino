8657fcf0fc83bdf490aa7f0f02115c56e75666d3
Pablo Garrido <pablogs9@gmail.com>
2020-11-25 10:08:08 +0100

    Update services

--- examples/micro-ros_addtwoints_service/micro-ros_addtwoints_service.ino
+++ examples/micro-ros_addtwoints_service/micro-ros_addtwoints_service.ino
@@ -14,7 +14,7 @@ rcl_allocator_t allocator;
 rclc_executor_t executor;
 
 rcl_service_options_t service_options;
-rcl_service_t serv;
+rcl_service_t service;
 const rosidl_service_type_support_t * service_type_support;
 rcl_wait_set_t wait_set;
 rmw_request_id_t req_id;
@@ -23,51 +23,48 @@ example_interfaces__srv__AddTwoInts_Request req;
 
 const char * service_name = "/add_two_ints";
 
+example_interfaces__srv__AddTwoInts_Request req;
+example_interfaces__srv__AddTwoInts_Response res;
+
+void service_callback(const void * req, rmw_request_id_t * req_id, void * res){
+  example_interfaces__srv__AddTwoInts_Request * req_in = (example_interfaces__srv__AddTwoInts_Request *) req;
+  example_interfaces__srv__AddTwoInts_Response * res_in = (example_interfaces__srv__AddTwoInts_Response *) res;
+
+  printf("Service request value: %d + %d. Seq %d\n", (int) req_in->a, (int) req_in->b, (int) req_id->sequence_number);
+
+  res_in->sum = req_in->a + req_in->b;
+}
+
 void setup() {
  delay(1000); 
 
- allocator = rcl_get_default_allocator();
+  allocator = rcl_get_default_allocator();
 
- options = rcl_get_zero_initialized_init_options();
- rcl_init_options_init(&options, rcl_get_default_allocator());
+  // create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
- rclc_support_init(&support, 0, NULL, &allocator);
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "add_twoints_client_rclc", "", &support));
 
- node_ops = rcl_node_get_default_options();
- node = rcl_get_zero_initialized_node();
- rclc_node_init_default(&node, "micro_ros_service_server_node", "", &support);
+  // create service
+  RCCHECK(rclc_service_init_default(&service, &node, ROSIDL_GET_SRV_TYPE_SUPPORT(example_interfaces, srv, AddTwoInts), "/addtwoints"));
 
- // init server
- service_options = rcl_service_get_default_options();
- serv = rcl_get_zero_initialized_service();
- service_type_support = ROSIDL_GET_SRV_TYPE_SUPPORT(example_interfaces, srv, AddTwoInts);
- rcl_service_init(&serv, &node, service_type_support, service_name, &service_options);
- wait_set = rcl_get_zero_initialized_wait_set();
- rcl_wait_set_init(&wait_set, 0, 0, 0, 1, 1, 0, &support.context, rcl_get_default_allocator());
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
 
- executor = rclc_executor_get_zero_initialized_executor();
- rclc_executor_init(&executor, &support.context, 1, &allocator);
+  unsigned int rcl_wait_timeout = 10;   // in ms
+  RCCHECK(rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout)));
+
+  RCCHECK(rclc_executor_add_service(&executor, &service, &req, &res, service_callback));
+
+  rclc_executor_spin(&executor);
+
+  RCCHECK(rcl_service_fini(&service, &node));
+  RCCHECK(rcl_node_fini(&node));
 }
 
 
 void loop() {
- delay(100);
- rcl_wait_set_clear(&wait_set);
-
- size_t index;
- rcl_wait_set_add_service(&wait_set, &serv, &index);
- 
- rcl_wait(&wait_set, RCL_MS_TO_NS(100));
- for (size_t i = 0; i < wait_set.size_of_services; i++) {
-   if (wait_set.services[i]) {
-     example_interfaces__srv__AddTwoInts_Request__init(&req);
-
-     rcl_take_request(&serv, &req_id, &req);
-
-     example_interfaces__srv__AddTwoInts_Response__init(&res);
-
-     res.sum = req.a + req.b;
-     rcl_send_response(&serv, &req_id, &res);
-   }
- }
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
