6075e06df4699ddb49786bf645a661da8eaa7bf3
Pablo Garrido <pablogs9@gmail.com>
2021-01-12 12:12:34 +0100

    Initial support (#66)

--- src/arduino_transports.c
+++ src/arduino_transports.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#if defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY32)
+#if defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY32) || defined(ARDUINO_TEENSY35)
 #include "teensy_transports.c.in"
 #elif defined(ARDUINO_ARCH_OPENCR)
 #include "opencr_transports.c.in"
 #endif
 
 // TODO: This should be fixed
-#if defined(ARDUINO_TEENSY32)
+#if defined(ARDUINO_TEENSY32) || defined(ARDUINO_TEENSY35)
 
 void _write(){
 }
