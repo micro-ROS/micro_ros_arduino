07492106b11c948ec0dbe6b1132eb31db44fcb76
pablogs9 <pablogs9@users.noreply.github.com>
2021-01-20 07:03:34 +0000

    micro-ROS rolling Library auto-update 20-01-2021 06:50

--- src/tracetools/tracetools.h
+++ src/tracetools/tracetools.h
@@ -1,4 +1,5 @@
 // Copyright 2019 Robert Bosch GmbH
+// Copyright 2020 Christophe Bedard
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -229,6 +230,18 @@ DECLARE_TRACEPOINT(
   const void * timer_handle,
   const void * callback)
 
+/// `rclcpp_timer_link_node`
+/**
+ * Link a timer to a node.
+ *
+ * \param[in] timer_handle pointer to the timer's `rcl_timer_t` handle
+ * \param[in] node_handle pointer to the `rcl_node_t` handle of the node the timer belongs to
+ */
+DECLARE_TRACEPOINT(
+  rclcpp_timer_link_node,
+  const void * timer_handle,
+  const void * node_handle)
+
 /// `rclcpp_callback_register`
 /**
  * Register a demangled function symbol with a callback.
@@ -269,6 +282,33 @@ DECLARE_TRACEPOINT(
 DECLARE_TRACEPOINT(
   callback_end,
   const void * callback)
+
+/// `rcl_lifecycle_state_machine_init`
+/**
+ * Lifecycle state machine initialisation.
+ * Links a `rcl_lifecycle_state_machine_t` handle to a `rcl_node_t` handle.
+ *
+ * \param[in] node_handle pointer to the node handle
+ * \param[in] state_machine pointer to the state machine
+ */
+DECLARE_TRACEPOINT(
+  rcl_lifecycle_state_machine_init,
+  const void * node_handle,
+  const void * state_machine)
+
+/// `rcl_lifecycle_transition`
+/**
+ * Lifecycle transition between two states.
+ *
+ * \param[in] state_machine pointer to the state machine for the transition
+ * \param[in] start_label start state label
+ * \param[in] goal_label goal state label
+ */
+DECLARE_TRACEPOINT(
+  rcl_lifecycle_transition,
+  const void * state_machine,
+  const char * start_label,
+  const char * goal_label)
 
 #ifdef __cplusplus
 }
