f5dfd38a03013b10c37e22e19b0dd4fbaaeeda45
Pablo Garrido <pablogs9@gmail.com>
2021-02-12 11:09:01 +0100

    Fix Arduino transports

--- src/default_transport.cpp
+++ src/default_transport.cpp
@@ -7,10 +7,10 @@ extern "C"
   #include <sys/time.h>
 
   int clock_gettime(clockid_t unused, struct timespec *tp) __attribute__ ((weak));
-  bool arduino_transport_open() __attribute__ ((weak));
-  bool arduino_transport_close() __attribute__ ((weak));
-  size_t arduino_transport_write(uint8_t *buf, size_t len, uint8_t *errcode) __attribute__ ((weak));
-  size_t arduino_transport_read(uint8_t *buf, size_t len, int timeout, uint8_t *errcode) __attribute__ ((weak));
+  bool arduino_transport_open(struct uxrCustomTransport * transport) __attribute__ ((weak));
+  bool arduino_transport_close(struct uxrCustomTransport * transport) __attribute__ ((weak));
+  size_t arduino_transport_write(struct uxrCustomTransport * transport, uint8_t *buf, size_t len, uint8_t *errcode) __attribute__ ((weak));
+  size_t arduino_transport_read(struct uxrCustomTransport * transport, uint8_t *buf, size_t len, int timeout, uint8_t *errcode) __attribute__ ((weak));
 
   int clock_gettime(clockid_t unused, struct timespec *tp)
   {
@@ -21,26 +21,26 @@ extern "C"
     return 0;
   }
 
-  bool arduino_transport_open()
+  bool arduino_transport_open(struct uxrCustomTransport * transport)
   {
     Serial.begin(115200);
     return true;
   }
 
-  bool arduino_transport_close()
+  bool arduino_transport_close(struct uxrCustomTransport * transport)
   {
     Serial.end();
     return true;
   }
 
-  size_t arduino_transport_write(uint8_t *buf, size_t len, uint8_t *errcode)
+  size_t arduino_transport_write(struct uxrCustomTransport * transport, uint8_t *buf, size_t len, uint8_t *errcode)
   {
     (void)errcode;
     size_t sent = Serial.write(buf, len);
     return sent;
   }
 
-  size_t arduino_transport_read(uint8_t *buf, size_t len, int timeout, uint8_t *errcode)
+  size_t arduino_transport_read(struct uxrCustomTransport * transport, uint8_t *buf, size_t len, int timeout, uint8_t *errcode)
   {
     (void)errcode;
     Serial.setTimeout(timeout);
