37155ee771a9683d3db8716a2871f63f73d48fcb
Pablo Garrido <pablogs9@gmail.com>
2021-03-05 11:38:07 +0100

    Fix Teensy transports (#158)

--- src/arduino_transports.c
+++ src/arduino_transports.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#if defined(ARDUINO_TEENSY41) || defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY32) || defined(ARDUINO_TEENSY35)
-#include "teensy_transports.c.in"
-#elif defined(ARDUINO_ARCH_OPENCR)
+#if defined(ARDUINO_ARCH_OPENCR)
 #include "opencr_transports.c.in"
 #endif
 
