841ffe7210ee8a254e20af9daf4e491719625270
Pablo Garrido <pablogs9@gmail.com>
2021-03-08 07:21:47 +0100

    Fix clock_gettime rollover (#159)
    * Fix clock_gettime rollover
    
    * Fix
    
    * Fix

--- src/default_transport.cpp
+++ src/default_transport.cpp
@@ -12,12 +12,25 @@ extern "C"
   size_t arduino_transport_write(struct uxrCustomTransport * transport, uint8_t *buf, size_t len, uint8_t *errcode) __attribute__ ((weak));
   size_t arduino_transport_read(struct uxrCustomTransport * transport, uint8_t *buf, size_t len, int timeout, uint8_t *errcode) __attribute__ ((weak));
 
+  #define micro_rollover_useconds 4294967295
+
   int clock_gettime(clockid_t unused, struct timespec *tp)
   {
     (void)unused;
+    static uint32_t rollover = 0;
+    static uint64_t last_measure = 0;
+
     uint64_t m = micros();
     tp->tv_sec = m / 1000000;
     tp->tv_nsec = (m % 1000000) * 1000;
+
+    // Rollover handling
+    rollover += (m < last_measure) ? 1 : 0;
+    uint64_t rollover_extra_us = rollover * micro_rollover_useconds;
+    tp->tv_sec += rollover_extra_us / 1000000;
+    tp->tv_nsec += (rollover_extra_us % 1000000) * 1000;
+    last_measure = m;
+
     return 0;
   }
 
