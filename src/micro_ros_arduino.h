fa5f82db4022483924644977c540418284fa6c66
Pablo Garrido <pablogs9@gmail.com>
2021-03-09 09:01:42 +0100

    Add wifi transport (#169)

--- src/micro_ros_arduino.h
+++ src/micro_ros_arduino.h
@@ -32,4 +32,41 @@ static inline void set_microros_transports(){
 	);
 }
 
+#ifdef TARGET_PORTENTA_H7_M7
+
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+extern "C" bool arduino_wifi_transport_open(struct uxrCustomTransport * transport);
+extern "C" bool arduino_wifi_transport_close(struct uxrCustomTransport * transport);
+extern "C" size_t arduino_wifi_transport_write(struct uxrCustomTransport* transport, const uint8_t * buf, size_t len, uint8_t * err);
+extern "C" size_t arduino_wifi_transport_read(struct uxrCustomTransport* transport, uint8_t* buf, size_t len, int timeout, uint8_t* err);
+
+struct micro_ros_agent_locator {
+	IPAddress address;
+	int port;
+};
+
+static inline void set_microros_wifi_transports(char * ssid, char * pass, char * agent_ip, uint agent_port){
+	while (WiFi.begin(ssid, pass) != WL_CONNECTED)
+	{
+    	delay(500);
+	}
+
+	static struct micro_ros_agent_locator locator;
+	locator.address.fromString(agent_ip);
+	locator.port = agent_port;
+
+	rmw_uros_set_custom_transport(
+		false,
+		(void *) &locator,
+		arduino_wifi_transport_open,
+		arduino_wifi_transport_close,
+		arduino_wifi_transport_write,
+		arduino_wifi_transport_read
+	);
+}
+
+#endif // TARGET_PORTENTA_H7_M7
+
 #endif  // MICRO_ROS_ARDUINO
