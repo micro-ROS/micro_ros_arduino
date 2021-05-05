ab0f61db9517aadf1f8810b0882c94ed9d0d1bb4
pablogs9 <pablogs9@users.noreply.github.com>
2021-05-05 10:54:21 +0000

    micro-ROS foxy Library auto-update 05-05-2021 10:37

---
+++ src/rmw_microros/discovery.h
@@ -0,0 +1,54 @@
+// Copyright 2021 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file
+ */
+
+#ifndef RMW_MICROROS__DISCOVERY_H_
+#define RMW_MICROROS__DISCOVERY_H_
+
+#include <rmw/rmw.h>
+#include <rmw/ret_types.h>
+#include <rmw/init_options.h>
+#include <rmw_microxrcedds_c/config.h>
+#include <ucdr/microcdr.h>
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif // if defined(__cplusplus)
+
+/** \addtogroup rmw micro-ROS RMW API
+ *  @{
+ */
+
+/**
+ * \brief Fills rmw implementation-specific options with the autodicovered address of an micro-ROS Agent.
+ *
+ * \param[in,out] rmw_options Updated options with rmw specifics.
+ * \return RMW_RET_OK If arguments were valid and set in rmw_init_options.
+ * \return RMW_RET_TIMEOUT If micro-ROS agent autodiscovery is timeout.
+ * \return RMW_RET_INVALID_ARGUMENT If rmw_init_options is not valid or unexpected arguments.
+ */
+rmw_ret_t rmw_uros_discover_agent(
+        rmw_init_options_t* rmw_options);
+
+/** @}*/
+
+#if defined(__cplusplus)
+}
+#endif // if defined(__cplusplus)
+
+#endif  // RMW_MICROROS__DISCOVERY_H_
---
+++ src/rmw_microros/time_sync.h
@@ -0,0 +1,73 @@
+// Copyright 2021 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file
+ */
+
+#ifndef RMW_MICROROS__TIME_SYNC_H_
+#define RMW_MICROROS__TIME_SYNC_H_
+
+#include <rmw/rmw.h>
+#include <rmw/ret_types.h>
+#include <rmw/init_options.h>
+#include <rmw/error_handling.h>
+#include <uxr/client/util/time.h>
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif // if defined(__cplusplus)
+
+/** \addtogroup rmw micro-ROS RMW API
+ *  @{
+ */
+
+/**
+ * \brief Returns the time synchronization state of the epoch time.
+ * \return true if last time synchronization succeded and false otherwise
+ */
+bool rmw_uros_epoch_synchronized();
+
+/**
+ * \brief Returns the epoch time in milliseconds taking into account the offset computed during the time synchronization.
+ * \return epoch time in milliseconds.
+ * \return 0 if session is not initialized.
+ */
+int64_t rmw_uros_epoch_millis();
+
+/**
+ * \brief Returns the epoch time in nanoseconds taking into account the offset computed during the time synchronization.
+ * \return epoch time in nanoseconds.
+ * \return 0 if session is not initialized.
+ */
+int64_t rmw_uros_epoch_nanos();
+
+/**
+ * \brief Synchronizes the session time using the NTP protocol.
+ * \param[in] timeout_ms The waiting time in milliseconds.
+ * \return RMW_RET_OK when success.
+ * \return RMW_RET_ERROR If no session is running or the synchronization fails.
+ */
+rmw_ret_t rmw_uros_sync_session(
+        const int timeout_ms);
+
+
+/** @}*/
+
+#if defined(__cplusplus)
+}
+#endif // if defined(__cplusplus)
+
+#endif  // RMW_MICROROS__TIME_SYNC_H_
