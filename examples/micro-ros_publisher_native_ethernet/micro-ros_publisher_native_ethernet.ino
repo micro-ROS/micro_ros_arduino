cf68ebc8b65c51fad662ef84d5480e61ccb36f22
Pablo Garrido <pablogs9@gmail.com>
2021-06-23 09:27:59 +0200

    Initial changes

---
+++ examples/micro-ros_publisher_native_ethernet/micro-ros_publisher_native_ethernet.ino
@@ -0,0 +1,102 @@
+#include <micro_ros_arduino.h>
+
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/int32.h>
+
+#ifndef ARDUINO_TEENSY41
+#error This example has only been tested on Teensy 4.1
+#endif
+
+// For this example, you might start the agent with something like this:
+// ros2 run micro_ros_agent micro_ros_agent udp4 -p 8888 -v 6
+
+// The implementation will attempt to assign the client IP address using DHCP
+// If DHCP fails, the following self assigned IP will be used instead
+IPAddress fallback_client_ip(192,168,1,100);
+IPAddress agent_ip(192,168,1,10);
+byte mac[] = {0x02, 0x47, 0x00, 0x00, 0x00, 0x01};
+uint agent_port = 8888;
+
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+#define LED_PIN 13
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+
+void error_loop(){
+  while(1){
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    delay(100);
+  }
+}
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    Serial.println("publishing");
+    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+    msg.data++;
+  }
+}
+
+void setup() {
+  if(!Serial) {
+    Serial.begin( 115200  );
+	delay(1000);
+  }
+
+  set_microros_native_ethernet_udp_transports( mac, fallback_client_ip, agent_ip, agent_port  );
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  delay(2000);
+
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "micro_ros_arduino_node_publisher"));
+
+  // create timer,
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+  msg.data = 0;
+}
+
+void loop() {
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+}
