cc198f1b02e3b02fffb9dce0e3e13c25278f6a09
pablogs9 <pablogs9@users.noreply.github.com>
2021-08-07 06:35:46 +0000

    micro-ROS rolling Library auto-update 07-08-2021 06:17

---
+++ src/test_msgs/msg/detail/bounded_plain_sequences__rosidl_typesupport_introspection_c.h
@@ -0,0 +1,26 @@
+// generated from rosidl_typesupport_introspection_c/resource/idl__rosidl_typesupport_introspection_c.h.em
+// with input from test_msgs:msg/BoundedPlainSequences.idl
+// generated code does not contain a copyright notice
+
+#ifndef TEST_MSGS__MSG__DETAIL__BOUNDED_PLAIN_SEQUENCES__ROSIDL_TYPESUPPORT_INTROSPECTION_C_H_
+#define TEST_MSGS__MSG__DETAIL__BOUNDED_PLAIN_SEQUENCES__ROSIDL_TYPESUPPORT_INTROSPECTION_C_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_typesupport_interface/macros.h"
+#include "test_msgs/msg/rosidl_typesupport_introspection_c__visibility_control.h"
+
+ROSIDL_TYPESUPPORT_INTROSPECTION_C_PUBLIC_test_msgs
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_introspection_c, test_msgs, msg, BoundedPlainSequences)();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // TEST_MSGS__MSG__DETAIL__BOUNDED_PLAIN_SEQUENCES__ROSIDL_TYPESUPPORT_INTROSPECTION_C_H_
