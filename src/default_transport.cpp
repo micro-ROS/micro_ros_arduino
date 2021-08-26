5c7ecd4b8dc6c63c70e1c007dbf928a324563a3b
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2021-08-26 10:28:50 +0200

    Fix rollover nanoseconds (#438)
    (cherry picked from commit 3e314f3e447a1ea7a8feb063a161205e7fae270f)

--- src/default_transport.cpp
+++ src/default_transport.cpp
@@ -18,17 +18,14 @@ extern "C"
   {
     (void)unused;
     static uint32_t rollover = 0;
-    static uint64_t last_measure = 0;
+    static uint32_t last_measure = 0;
 
-    uint64_t m = micros();
-    tp->tv_sec = m / 1000000;
-    tp->tv_nsec = (m % 1000000) * 1000;
-
-    // Rollover handling
+    uint32_t m = micros();
     rollover += (m < last_measure) ? 1 : 0;
-    uint64_t rollover_extra_us = rollover * micro_rollover_useconds;
-    tp->tv_sec += rollover_extra_us / 1000000;
-    tp->tv_nsec += (rollover_extra_us % 1000000) * 1000;
+
+    uint64_t real_us = (uint64_t) (m + rollover * micro_rollover_useconds);
+    tp->tv_sec = real_us / 1000000;
+    tp->tv_nsec = (real_us % 1000000) * 1000;
     last_measure = m;
 
     return 0;
