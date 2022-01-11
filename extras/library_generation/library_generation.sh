2840b321e6a2c7c3b5286f4d8fdb826fefe77b80
acuadros95 <acuadros1995@gmail.com>
2022-01-11 17:13:16 +0100

    Revert commit and add pip3

--- extras/library_generation/library_generation.sh
+++ extras/library_generation/library_generation.sh
@@ -53,171 +53,117 @@ pushd firmware/mcu_ws > /dev/null
 
 popd > /dev/null
 
+# Workaround. Remove when https://github.com/ros2/rosidl/pull/596 is merged
+touch firmware/mcu_ws/ros2/common_interfaces/actionlib_msgs/COLCON_IGNORE;
+touch firmware/mcu_ws/ros2/common_interfaces/std_srvs/COLCON_IGNORE;
+touch firmware/mcu_ws/ros2/example_interfaces/COLCON_IGNORE;
+
 ######## Clean and source ########
 find /project/src/ ! -name micro_ros_arduino.h ! -name *.c ! -name *.cpp ! -name *.c.in -delete
-
-cd firmware
-echo "" > /project/built_packages
-for f in $(find $(pwd) -name .git -type d); do pushd $f > /dev/null; echo $(git config --get remote.origin.url) $(git rev-parse HEAD) >> /project/built_packages; popd > /dev/null; done;
-
-cd /project
-if [[ `git diff-index --name-only HEAD | grep built_packages2` ]]; then
-    CHANGES=1
-    echo "Changes detected"
-else
-    CHANGES=0
-    echo "No changes detected"
-fi
-
-cd /uros_ws
 
 ######## Build for OpenCR  ########
 if [[ " ${PLATFORMS[@]} " =~ " opencr1 " ]]; then
     rm -rf firmware/build
-    FILE=/project/src/cortex-m7/fpv5-sp-d16-softfp/libmicroros.a
 
-    if ! [[ -f "/project/src/cortex-m7/fpv5-sp-d16-softfp/libmicroros.a" -a $CHANGES ]]; then
-        echo "Building opencr1 library"
-        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-5_4-2016q2/bin/arm-none-eabi-
-        ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/opencr_toolchain.cmake /project/extras/library_generation/colcon.meta
+    export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-5_4-2016q2/bin/arm-none-eabi-
+    ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/opencr_toolchain.cmake /project/extras/library_generation/colcon.meta
 
-        find firmware/build/include/ -name "*.c"  -delete
-        cp -R firmware/build/include/* /project/src/
+    find firmware/build/include/ -name "*.c"  -delete
+    cp -R firmware/build/include/* /project/src/
 
-        mkdir -p /project/src/cortex-m7/fpv5-sp-d16-softfp
-        cp -R firmware/build/libmicroros.a /project/src/cortex-m7/fpv5-sp-d16-softfp/libmicroros.a
-    else
-        echo "Skip opencr1 build"
-    fi
+    mkdir -p /project/src/cortex-m7/fpv5-sp-d16-softfp
+    cp -R firmware/build/libmicroros.a /project/src/cortex-m7/fpv5-sp-d16-softfp/libmicroros.a
 fi
 
 ######## Build for SAMD (e.g. Arduino Zero) ########
 if [[ " ${PLATFORMS[@]} " =~ " cortex_m0 " ]]; then
     rm -rf firmware/build
-    FILE=/project/src/cortex-m0plus/libmicroros.a
 
-    if [[ ! -f $FILE || (-f $FILE && $CHANGES -eq 1 ) ]]; then
-        echo "Building cortex_m0 library"
-        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-7-2017-q4-major/bin/arm-none-eabi-
-        ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/cortex_m0_toolchain.cmake /project/extras/library_generation/colcon_verylowmem.meta
+    export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-7-2017-q4-major/bin/arm-none-eabi-
+    ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/cortex_m0_toolchain.cmake /project/extras/library_generation/colcon_verylowmem.meta
 
-        find firmware/build/include/ -name "*.c"  -delete
-        cp -R firmware/build/include/* /project/src/
+    find firmware/build/include/ -name "*.c"  -delete
+    cp -R firmware/build/include/* /project/src/
 
-        mkdir -p /project/src/cortex-m0plus
-        cp -R firmware/build/libmicroros.a /project/src/cortex-m0plus/libmicroros.a
-    else
-        echo "Skip cortex_m0 build"
-    fi
+    mkdir -p /project/src/cortex-m0plus
+    cp -R firmware/build/libmicroros.a /project/src/cortex-m0plus/libmicroros.a
 fi
 
 ######## Build for SAM (e.g. Arduino Due) ########
 if [[ " ${PLATFORMS[@]} " =~ " cortex_m3 " ]]; then
     rm -rf firmware/build
-    FILE=/project/src/cortex-m3/libmicroros.a
 
-    if [[ ! -f $FILE || (-f $FILE && $CHANGES -eq 1 ) ]]; then
-        echo "Building cortex_m3 library"
-        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-9-2020-q2-update
-        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-4_8-2014q1/bin/arm-none-eabi-
-        ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/cortex_m3_toolchain.cmake /project/extras/library_generation/colcon_lowmem.meta
+    export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-4_8-2014q1/bin/arm-none-eabi-
+    ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/cortex_m3_toolchain.cmake /project/extras/library_generation/colcon_lowmem.meta
 
-        find firmware/build/include/ -name "*.c"  -delete
-        cp -R firmware/build/include/* /project/src/
+    find firmware/build/include/ -name "*.c"  -delete
+    cp -R firmware/build/include/* /project/src/
 
-        mkdir -p /project/src/cortex-m3
-        cp -R firmware/build/libmicroros.a /project/src/cortex-m3/libmicroros.a
-    else
-        echo "Skip cortex_m3 build"
-    fi
+    mkdir -p /project/src/cortex-m3
+    cp -R firmware/build/libmicroros.a /project/src/cortex-m3/libmicroros.a
 fi
 
 ######## Build for STM32F4 ########
 if [[ " ${PLATFORMS[@]} " =~ " cortex_m4 " ]]; then
     rm -rf firmware/build
-    FILE=/project/src/cortex-m4/libmicroros.a
 
-    if [[ ! -f $FILE || (-f $FILE && $CHANGES -eq 1 ) ]]; then
-        echo "Building cortex_m4 library"
-        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-9-2020-q2-update/bin/arm-none-eabi-
-        ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/cortex_m4_toolchain.cmake /project/extras/library_generation/colcon_lowmem.meta
+    export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-9-2020-q2-update/bin/arm-none-eabi-
+    ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/cortex_m4_toolchain.cmake /project/extras/library_generation/colcon_lowmem.meta
 
-        find firmware/build/include/ -name "*.c"  -delete
-        cp -R firmware/build/include/* /project/src/
+    find firmware/build/include/ -name "*.c"  -delete
+    cp -R firmware/build/include/* /project/src/
 
-        mkdir -p /project/src/cortex-m4
-        cp -R firmware/build/libmicroros.a /project/src/cortex-m4/libmicroros.a
-    else
-        echo "Skip cortex_m4 build"
-    fi
+    mkdir -p /project/src/cortex-m4
+    cp -R firmware/build/libmicroros.a /project/src/cortex-m4/libmicroros.a
 fi
 
 ######## Build for Teensy 3.2 ########
 if [[ " ${PLATFORMS[@]} " =~ " teensy32 " ]]; then
     rm -rf firmware/build
-    FILE=/project/src/mk20dx256/libmicroros.a
 
-    if [[ ! -f $FILE || (-f $FILE && $CHANGES -eq 1 ) ]]; then
-        echo "Building teensy31 library"
-        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-
-        ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/teensy32_toolchain.cmake /project/extras/library_generation/colcon_lowmem.meta
+    export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-
+    ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/teensy32_toolchain.cmake /project/extras/library_generation/colcon_lowmem.meta
 
-        find firmware/build/include/ -name "*.c"  -delete
-        cp -R firmware/build/include/* /project/src/
+    find firmware/build/include/ -name "*.c"  -delete
+    cp -R firmware/build/include/* /project/src/
 
-        mkdir -p /project/src/mk20dx256
-        cp -R firmware/build/libmicroros.a /project/src/mk20dx256/libmicroros.a
-    else
-        echo "Skip teensy32 build"
-    fi
+    mkdir -p /project/src/mk20dx256
+    cp -R firmware/build/libmicroros.a /project/src/mk20dx256/libmicroros.a
 fi
 
 ######## Build for Teensy 3.5 ########
 if [[ " ${PLATFORMS[@]} " =~ " teensy35 " ]]; then
     rm -rf firmware/build
-    FILE=/project/src/mk64fx512/fpv4-sp-d16-hard/libmicroros.a
 
-    if [[ ! -f $FILE || (-f $FILE && $CHANGES -eq 1 ) ]]; then
-        echo "Building teensy35 library"
-        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-
-        ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/teensy35_toolchain.cmake /project/extras/library_generation/colcon_lowmem.meta
+    export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-
+    ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/teensy35_toolchain.cmake /project/extras/library_generation/colcon_lowmem.meta
 
-        find firmware/build/include/ -name "*.c"  -delete
-        cp -R firmware/build/include/* /project/src/
+    find firmware/build/include/ -name "*.c"  -delete
+    cp -R firmware/build/include/* /project/src/
 
-        mkdir -p /project/src/mk64fx512/fpv4-sp-d16-hard
-        cp -R firmware/build/libmicroros.a /project/src/mk64fx512/fpv4-sp-d16-hard/libmicroros.a
-    else
-        echo "Skip teensy35 build"
-    fi
+    mkdir -p /project/src/mk64fx512/fpv4-sp-d16-hard
+    cp -R firmware/build/libmicroros.a /project/src/mk64fx512/fpv4-sp-d16-hard/libmicroros.a
 fi
 
 ######## Build for Teensy 4 ########
 if [[ " ${PLATFORMS[@]} " =~ " teensy4 " ]]; then
     rm -rf firmware/build
-    FILE=/project/src/imxrt1062/fpv5-d16-hard/libmicroros.a
 
-    if [[ ! -f $FILE || (-f $FILE && $CHANGES -eq 1 ) ]]; then
-        echo "Building teensy4 library"
-        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-
-        ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/teensy4_toolchain.cmake /project/extras/library_generation/colcon.meta
+    export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-
+    ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/teensy4_toolchain.cmake /project/extras/library_generation/colcon.meta
 
-        find firmware/build/include/ -name "*.c"  -delete
-        cp -R firmware/build/include/* /project/src/
+    find firmware/build/include/ -name "*.c"  -delete
+    cp -R firmware/build/include/* /project/src/
 
-        mkdir -p /project/src/imxrt1062/fpv5-d16-hard
-        cp -R firmware/build/libmicroros.a /project/src/imxrt1062/fpv5-d16-hard/libmicroros.a
-    else
-        echo "Skip teensy4 library"
-    fi
+    mkdir -p /project/src/imxrt1062/fpv5-d16-hard
+    cp -R firmware/build/libmicroros.a /project/src/imxrt1062/fpv5-d16-hard/libmicroros.a
 fi
 
 ######## Build for Arduino Portenta M4 core ########
 # if [[ " ${PLATFORMS[@]} " =~ " portenta-m4 " ]]; then
-#    rm -rf firmware/build
+#     rm -rf firmware/build
 
-#    if ! [[ -f "/project/src/cortex-m7/fpv5-sp-d16-softfp/libmicroros.a" -a ! $CHANGES ]]; then
-#        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-7-2017-q4-major/bin/arm-none-eabi-
+#     export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-7-2017-q4-major/bin/arm-none-eabi-
 #     ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/portenta-m4_toolchain.cmake /project/extras/library_generation/colcon.meta
 
 #     find firmware/build/include/ -name "*.c"  -delete
@@ -230,66 +176,52 @@ fi
 ######## Build for Arduino Portenta M7 core ########
 if [[ " ${PLATFORMS[@]} " =~ " portenta-m7 " ]]; then
     rm -rf firmware/build
-    FILE=/project/src/cortex-m7/fpv5-d16-softfp/libmicroros.a
 
-    if [[ ! -f $FILE || (-f $FILE && $CHANGES -eq 1 ) ]]; then
-        echo "Building portenta-m7 library"
-        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-7-2017-q4-major/bin/arm-none-eabi-
-        ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/portenta-m7_toolchain.cmake /project/extras/library_generation/colcon.meta
+    export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-7-2017-q4-major/bin/arm-none-eabi-
+    ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/portenta-m7_toolchain.cmake /project/extras/library_generation/colcon.meta
 
-        find firmware/build/include/ -name "*.c"  -delete
-        cp -R firmware/build/include/* /project/src/
+    find firmware/build/include/ -name "*.c"  -delete
+    cp -R firmware/build/include/* /project/src/
 
-        mkdir -p /project/src/cortex-m7/fpv5-d16-softfp
-        cp -R firmware/build/libmicroros.a /project/src/cortex-m7/fpv5-d16-softfp/libmicroros.a
-    else
-        echo "Skip portenta-m7 build"
-    fi
+    mkdir -p /project/src/cortex-m7/fpv5-d16-softfp
+    cp -R firmware/build/libmicroros.a /project/src/cortex-m7/fpv5-d16-softfp/libmicroros.a
 fi
 
 ######## Build for Kakute F7 M7 core  ########
 if [[ " ${PLATFORMS[@]} " =~ " kakutef7-m7 " ]]; then
     rm -rf firmware/build
-    FILE=/project/src/cortex-m7/fpv5-sp-d16-hardfp/libmicroros.a
 
-    if [[ ! -f $FILE || (-f $FILE && $CHANGES -eq 1 ) ]]; then
-        echo "Building kakutef7-m7 library"
-        export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-9-2020-q2-update/bin/arm-none-eabi-
-        ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/kakutef7-m7_toolchain.cmake /project/extras/library_generation/colcon.meta
+    export TOOLCHAIN_PREFIX=/uros_ws/gcc-arm-none-eabi-9-2020-q2-update/bin/arm-none-eabi-
+    ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/kakutef7-m7_toolchain.cmake /project/extras/library_generation/colcon.meta
 
-        find firmware/build/include/ -name "*.c"  -delete
-        cp -R firmware/build/include/* /project/src/
+    find firmware/build/include/ -name "*.c"  -delete
+    cp -R firmware/build/include/* /project/src/
 
-        mkdir -p /project/src/cortex-m7/fpv5-sp-d16-hardfp
-        cp -R firmware/build/libmicroros.a /project/src/cortex-m7/fpv5-sp-d16-hardfp/libmicroros.a
-    else
-        echo "Skip kakutef7-m7 build"
-    fi
+    mkdir -p /project/src/cortex-m7/fpv5-sp-d16-hardfp
+    cp -R firmware/build/libmicroros.a /project/src/cortex-m7/fpv5-sp-d16-hardfp/libmicroros.a
 fi
 
 ######## Build for ESP 32  ########
 if [[ " ${PLATFORMS[@]} " =~ " esp32 " ]]; then
     rm -rf firmware/build
-    FILE=/project/src/esp32/libmicroros.a
 
-    if [[ ! -f $FILE || (-f $FILE && $CHANGES -eq 1 ) ]]; then
-        echo "Building esp32 library"
-        export TOOLCHAIN_PREFIX=/uros_ws/xtensa-esp32-elf/bin/xtensa-esp32-elf-
-        ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/esp32_toolchain.cmake /project/extras/library_generation/colcon.meta
+    export TOOLCHAIN_PREFIX=/uros_ws/xtensa-esp32-elf/bin/xtensa-esp32-elf-
+    ros2 run micro_ros_setup build_firmware.sh /project/extras/library_generation/esp32_toolchain.cmake /project/extras/library_generation/colcon.meta
 
-        find firmware/build/include/ -name "*.c"  -delete
-        cp -R firmware/build/include/* /project/src/
+    find firmware/build/include/ -name "*.c"  -delete
+    cp -R firmware/build/include/* /project/src/
 
-        mkdir -p /project/src/esp32
-        cp -R firmware/build/libmicroros.a /project/src/esp32/libmicroros.a
-    else
-        echo "Skip esp32 build"
-    fi
+    mkdir -p /project/src/esp32
+    cp -R firmware/build/libmicroros.a /project/src/esp32/libmicroros.a
 fi
 
 ######## Generate extra files ########
 find firmware/mcu_ws/ros2 \( -name "*.srv" -o -name "*.msg" -o -name "*.action" \) | awk -F"/" '{print $(NF-2)"/"$NF}' > /project/available_ros2_types
 find firmware/mcu_ws/extra_packages \( -name "*.srv" -o -name "*.msg" -o -name "*.action" \) | awk -F"/" '{print $(NF-2)"/"$NF}' >> /project/available_ros2_types
+
+cd firmware
+echo "" > /project/built_packages
+for f in $(find $(pwd) -name .git -type d); do pushd $f > /dev/null; echo $(git config --get remote.origin.url) $(git rev-parse HEAD) >> /project/built_packages; popd > /dev/null; done;
 
 ######## Fix permissions ########
 sudo chmod -R 777 .
