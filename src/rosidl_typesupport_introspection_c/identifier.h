1b94d1171cdc384da15723f520116c4cd3c42d7d
pablogs9 <pablogs9@users.noreply.github.com>
2022-02-02 06:53:58 +0000

    micro-ROS rolling Library auto-update 02-02-2022 06:17

--- src/rosidl_typesupport_introspection_c/identifier.h
+++ src/rosidl_typesupport_introspection_c/identifier.h
@@ -22,6 +22,7 @@ extern "C"
 {
 #endif
 
+/// String identifying the typesupport introspection implementation in use.
 ROSIDL_TYPESUPPORT_INTROSPECTION_C_PUBLIC
 extern const char * rosidl_typesupport_introspection_c__identifier;
 
