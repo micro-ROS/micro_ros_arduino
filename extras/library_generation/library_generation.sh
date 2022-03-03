f8f7344099cf59161fcb690c2650879c88d24f0c
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2022-03-03 08:44:35 +0100

    Disable changes check (#794)
    (cherry picked from commit e4a7fb762084a32998733917c5e51dafd4768b07)

--- extras/library_generation/library_generation.sh
+++ extras/library_generation/library_generation.sh
@@ -54,20 +54,6 @@ pushd firmware/mcu_ws > /dev/null
 
 popd > /dev/null
 
-cd firmware
-echo "" > /project/built_packages
-for f in $(find $(pwd) -name .git -type d); do pushd $f > /dev/null; echo $(git config --get remote.origin.url) $(git rev-parse HEAD) >> /project/built_packages; popd > /dev/null; done;
-
-cd /project
-if [[ `git diff-index --name-only HEAD | grep built_packages` ]]; then
-    echo "Changes detected"
-else
-    echo "No changes detected"
-    exit 0
-fi
-
-cd /uros_ws
-
 ######## Clean and source ########
 find /project/src/ ! -name micro_ros_arduino.h ! -name *.c ! -name *.cpp ! -name *.c.in -delete
 
