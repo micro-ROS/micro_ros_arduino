505d6ab86f12e2eeb784c97d4480c3eaa4f55e89
Jenifen <46198024+Jenifen@users.noreply.github.com>
2022-03-21 14:55:45 +0100

    update mac address
    Co-authored-by: Pablo Garrido <pablogs9@gmail.com>

--- examples/micro-ros_publisher_ethernet/micro-ros_publisher_ethernet.ino
+++ examples/micro-ros_publisher_ethernet/micro-ros_publisher_ethernet.ino
@@ -41,7 +41,7 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 }
 
 void setup() {
-  byte arduino_mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+  byte arduino_mac[] = { 0xAA, 0xBB, 0xCC, 0xEE, 0xDD, 0xFF };
   // The IP address will be dependent on your local network
   IPAddress arduino_ip(192, 168, 1, 177);
   IPAddress agent_ip(192, 168, 1, 113);
