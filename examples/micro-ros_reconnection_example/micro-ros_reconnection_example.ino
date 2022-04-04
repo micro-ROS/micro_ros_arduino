abda4d30182c73de53005f04b526c655d0542013
Pablo Garrido <pablogs9@gmail.com>
2022-04-04 08:26:49 +0200

    FIx indentation
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>

--- examples/micro-ros_reconnection_example/micro-ros_reconnection_example.ino
+++ examples/micro-ros_reconnection_example/micro-ros_reconnection_example.ino
@@ -12,9 +12,9 @@
 #define LED_PIN 13
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){return false;}}
 #define EXECUTE_EVERY_N_MS(MS, X)  do { \
-	static volatile int64_t init = -1; \
-	if (init == -1) { init = uxr_millis();} \
-	if (uxr_millis() - init > MS) { X; init = uxr_millis();} \
+  static volatile int64_t init = -1; \
+  if (init == -1) { init = uxr_millis();} \
+  if (uxr_millis() - init > MS) { X; init = uxr_millis();} \
 } while (0)\
 
 rclc_support_t support;
@@ -27,19 +27,19 @@ std_msgs__msg__Int32 msg;
 bool micro_ros_init_successful;
 
 enum states {
-	WAITING_AGENT,
-	AGENT_AVAILABLE,
-	AGENT_CONNECTED,
-	AGENT_DISCONNECTED
+  WAITING_AGENT,
+  AGENT_AVAILABLE,
+  AGENT_CONNECTED,
+  AGENT_DISCONNECTED
 } state;
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
-	(void) last_call_time;
-	if (timer != NULL) {
-		rcl_publish(&publisher, &msg, NULL);
-		msg.data++;
-	}
+  (void) last_call_time;
+  if (timer != NULL) {
+    rcl_publish(&publisher, &msg, NULL);
+    msg.data++;
+  }
 }
 
 // Functions create_entities and destroy_entities can take several seconds.
@@ -49,33 +49,33 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 
 bool create_entities()
 {
-	allocator = rcl_get_default_allocator();
+  allocator = rcl_get_default_allocator();
 
-	// create init_options
-	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  // create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
-	// create node
-	RCCHECK(rclc_node_init_default(&node, "int32_publisher_rclc", "", &support));
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "int32_publisher_rclc", "", &support));
 
-	// create publisher
-	RCCHECK(rclc_publisher_init_best_effort(
-		&publisher,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"std_msgs_msg_Int32"));
+  // create publisher
+  RCCHECK(rclc_publisher_init_best_effort(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "std_msgs_msg_Int32"));
 
-	// create timer,
-	const unsigned int timer_timeout = 1000;
-	RCCHECK(rclc_timer_init_default(
-		&timer,
-		&support,
-		RCL_MS_TO_NS(timer_timeout),
-		timer_callback));
+  // create timer,
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
 
-	// create executor
-	executor = rclc_executor_get_zero_initialized_executor();
-	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-	RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  // create executor
+  executor = rclc_executor_get_zero_initialized_executor();
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
   return true;
 }
@@ -83,13 +83,13 @@ bool create_entities()
 void destroy_entities()
 {
   rmw_context_t * rmw_context = rcl_context_get_rmw_context(&support.context);
-	(void) rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
+  (void) rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
 
-	rcl_publisher_fini(&publisher, &node);
-	rcl_timer_fini(&timer);
-	rclc_executor_fini(&executor);
+  rcl_publisher_fini(&publisher, &node);
+  rcl_timer_fini(&timer);
+  rclc_executor_fini(&executor);
   rcl_node_fini(&node);
-	rclc_support_fini(&support);
+  rclc_support_fini(&support);
 }
 
 void setup() {
@@ -102,35 +102,35 @@ void setup() {
 }
 
 void loop() {
-	switch (state) {
-		case WAITING_AGENT:
-			EXECUTE_EVERY_N_MS(500, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_AVAILABLE : WAITING_AGENT;);
-			break;
-		case AGENT_AVAILABLE:
-			state = (true == create_entities()) ? AGENT_CONNECTED : WAITING_AGENT;
-			if (state == WAITING_AGENT) {
-				destroy_entities();
-			};
-			break;
-		case AGENT_CONNECTED:
-			EXECUTE_EVERY_N_MS(200, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;);
-			if (state == AGENT_CONNECTED) {
-				rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
-			}
-			break;
-		case AGENT_DISCONNECTED:
-			destroy_entities();
-			state = WAITING_AGENT;
-			break;
-		default:
-			break;
-	}
+  switch (state) {
+    case WAITING_AGENT:
+      EXECUTE_EVERY_N_MS(500, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_AVAILABLE : WAITING_AGENT;);
+      break;
+    case AGENT_AVAILABLE:
+      state = (true == create_entities()) ? AGENT_CONNECTED : WAITING_AGENT;
+      if (state == WAITING_AGENT) {
+        destroy_entities();
+      };
+      break;
+    case AGENT_CONNECTED:
+      EXECUTE_EVERY_N_MS(200, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;);
+      if (state == AGENT_CONNECTED) {
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+      }
+      break;
+    case AGENT_DISCONNECTED:1
+      destroy_entities();
+      state = WAITING_AGENT;
+      break;
+    default:
+      break;
+  }
 
-	if (state == AGENT_CONNECTED) {
-		digitalWrite(LED_PIN, 1);
-	} else {
-		digitalWrite(LED_PIN, 0);
-	}
+  if (state == AGENT_CONNECTED) {
+    digitalWrite(LED_PIN, 1);
+  } else {
+    digitalWrite(LED_PIN, 0);
+  }
 
-  	delayMicroseconds(delay);
+    delayMicroseconds(delay);
 }
