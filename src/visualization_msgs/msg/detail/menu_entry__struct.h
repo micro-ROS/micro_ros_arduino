26603c528c39ca9ded09b87e82d80538d134c269
pablogs9 <pablogs9@users.noreply.github.com>
2022-04-12 06:50:37 +0000

    micro-ROS rolling Library auto-update 12-04-2022 06:20

--- src/visualization_msgs/msg/detail/menu_entry__struct.h
+++ src/visualization_msgs/msg/detail/menu_entry__struct.h
@@ -62,16 +62,16 @@ enum
   *
   * Example:
   * - id = 3
-  * parent_id = 0
+  *   parent_id = 0
   * title = "fun"
   * - id = 2
-  *   parent_id = 0
-  *   title = "robot"
+  * parent_id = 0
+  * title = "robot"
   * - id = 4
-  * parent_id = 2
-  *   title = "pr2"
+  *   parent_id = 2
+  * title = "pr2"
   * - id = 5
-  * parent_id = 2
+  *   parent_id = 2
   * title = "turtle"
   *
   * Gives a menu tree like this:
