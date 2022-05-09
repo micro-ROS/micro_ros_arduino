c846794cca04ae9d8f03352bd6af1985822d6ffe
pablogs9 <pablogs9@users.noreply.github.com>
2022-05-09 06:52:13 +0000

    micro-ROS rolling Library auto-update 09-05-2022 06:22

--- src/actionlib_msgs/msg/detail/goal_status__struct.h
+++ src/actionlib_msgs/msg/detail/goal_status__struct.h
@@ -38,7 +38,7 @@ enum
 /// Constant 'PREEMPTED'.
 /**
   * The goal received a cancel request after it started executing
-  * and has since completed its execution (Terminal State).
+  *   and has since completed its execution (Terminal State).
  */
 enum
 {
@@ -48,7 +48,7 @@ enum
 /// Constant 'SUCCEEDED'.
 /**
   * The goal was achieved successfully by the action server
-  *   (Terminal State).
+  * (Terminal State).
  */
 enum
 {
@@ -58,7 +58,7 @@ enum
 /// Constant 'ABORTED'.
 /**
   * The goal was aborted during execution by the action server due
-  * to some failure (Terminal State).
+  *    to some failure (Terminal State).
  */
 enum
 {
@@ -78,7 +78,7 @@ enum
 /// Constant 'PREEMPTING'.
 /**
   * The goal received a cancel request after it started executing
-  *    and has not yet completed execution.
+  * and has not yet completed execution.
  */
 enum
 {
@@ -88,7 +88,7 @@ enum
 /// Constant 'RECALLING'.
 /**
   * The goal received a cancel request before it started executing, but
-  *    the action server has not yet confirmed that the goal is canceled.
+  * the action server has not yet confirmed that the goal is canceled.
  */
 enum
 {
@@ -98,7 +98,7 @@ enum
 /// Constant 'RECALLED'.
 /**
   * The goal received a cancel request before it started executing
-  *    and was successfully cancelled (Terminal State).
+  * and was successfully cancelled (Terminal State).
  */
 enum
 {
@@ -108,7 +108,7 @@ enum
 /// Constant 'LOST'.
 /**
   * An action client can determine that a goal is LOST. This should not
-  * be sent over the wire by an action server.
+  *    be sent over the wire by an action server.
  */
 enum
 {
