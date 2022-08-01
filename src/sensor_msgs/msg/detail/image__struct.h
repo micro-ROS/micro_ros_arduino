3120b9b923ac81a73ff198d35a54e73831e76c41
pablogs9 <pablogs9@users.noreply.github.com>
2022-08-01 07:03:41 +0000

    micro-ROS rolling Library auto-update 01-08-2022 06:33

--- src/sensor_msgs/msg/detail/image__struct.h
+++ src/sensor_msgs/msg/detail/image__struct.h
@@ -46,7 +46,7 @@ typedef struct sensor_msgs__msg__Image
   uint32_t height;
   /// image width, that is, number of columns
   uint32_t width;
-  /// The legal values for encoding are in file src/image_encodings.cpp
+  /// The legal values for encoding are in file include/sensor_msgs/image_encodings.hpp
   /// If you want to standardize a new string format, join
   /// ros-users@lists.ros.org and send an email proposing a new encoding.
   /// Encoding of pixels -- channel meaning, ordering, size
