1b814066056c7fad253735c81ecf31a15eb5b844
pablogs9 <pablogs9@users.noreply.github.com>
2022-12-07 06:57:06 +0000

    micro-ROS rolling Library auto-update 07-12-2022 06:19

--- src/rcl/time.h
+++ src/rcl/time.h
@@ -168,10 +168,15 @@ typedef struct rcl_time_point_s
 // } rcl_rate_t;
 // TODO(tfoote) integrate rate and timer implementations
 
-/// Check if the time point value is valid.
+/// Check if the clock has started.
 /**
- * This function returns true if the time point value is non-zero.
+ * This function returns true if the clock contains a time point value
+ * that is non-zero.
  * Note that if data is uninitialized it may give a false positive.
+ *
+ * This function is primarily used to check if a clock using ROS time
+ * has started. This is because it is possible that a simulator might be
+ * initialized paused, causing ROS time to be 0 until it is unpaused.
  *
  * <hr>
  * Attribute          | Adherence
@@ -181,35 +186,13 @@ typedef struct rcl_time_point_s
  * Uses Atomics       | No
  * Lock-Free          | Yes
  *
- * \param[in] time_point_value the time point value which is being queried
- * \return true if the source is believed to be valid, otherwise return false.
+ * \param[in] clock the handle to the clock which is being queried
+ * \return true if the clock has started, otherwise return false.
  */
 RCL_PUBLIC
 RCL_WARN_UNUSED
 bool
-rcl_time_point_value_valid(rcl_time_point_value_t time_point_value);
-
-/// Check if the time point is valid.
-/**
- * This function returns true if the time point appears to be valid.
- * It will check that the type is not uninitialized, and that the time point value is non-zero.
- * Note that if data is uninitialized it may give a false positive.
- *
- * <hr>
- * Attribute          | Adherence
- * ------------------ | -------------
- * Allocates Memory   | No
- * Thread-Safe        | Yes
- * Uses Atomics       | No
- * Lock-Free          | Yes
- *
- * \param[in] time_point the time point which is being queried
- * \return true if the source is believed to be valid, otherwise return false.
- */
-RCL_PUBLIC
-RCL_WARN_UNUSED
-bool
-rcl_time_point_valid(rcl_time_point_t * time_point);
+rcl_clock_time_started(rcl_clock_t * clock);
 
 /// Check if the clock has valid values.
 /**
