d82a390af7f46c441a95adeb5bc0a0b45550d532
nick weldin <n.weldin@mdx.ac.uk>
2024-02-19 19:20:11 +0000

    removed timer and executor, as not used by code

--- examples/micro-ros_tf_publisher/micro-ros_tf_publisher.ino
+++ examples/micro-ros_tf_publisher/micro-ros_tf_publisher.ino
@@ -9,7 +9,6 @@
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
-#include <rclc/executor.h>
 
 #include <geometry_msgs/msg/transform_stamped.h>
 #include <tf2_msgs/msg/tf_message.h>
@@ -19,11 +18,9 @@
 
 rcl_publisher_t publisher;
 tf2_msgs__msg__TFMessage tf_message;
-rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t node;
-rcl_timer_t timer;
 
 #define LED_PIN 13
 
@@ -55,12 +52,6 @@ void error_loop(){
   }
 }
 
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
-{
-  RCLC_UNUSED(last_call_time);
-  RCLC_UNUSED(timer);
-}
-
 void setup() {
   set_microros_transports();
 
@@ -71,7 +62,6 @@ void setup() {
   {
     IMU.update();
   }
-
 
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
@@ -93,16 +83,6 @@ void setup() {
     ROSIDL_GET_MSG_TYPE_SUPPORT(tf2_msgs, msg, TFMessage),
     "/tf"));
 
-  // create timer,
-  const unsigned int timer_timeout = 1000;
-  RCCHECK(rclc_timer_init_default(
-    &timer,
-    &support,
-    RCL_MS_TO_NS(timer_timeout),
-    timer_callback));
-
-  // create executor
-  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
 
   if(!micro_ros_utilities_create_message_memory(
       ROSIDL_GET_MSG_TYPE_SUPPORT(tf2_msgs, msg, TFMessage),
@@ -139,6 +119,4 @@ void loop() {
 	tf_message.transforms.data[0].header.stamp.sec = tv.tv_sec;
 
   RCSOFTCHECK(rcl_publish(&publisher, &tf_message, NULL));
-
-  //RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
